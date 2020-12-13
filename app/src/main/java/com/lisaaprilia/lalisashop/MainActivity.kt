d592efadd107a94511bca62ee8528d5b7550b089
LISA <aprilialisa654@gmail.com>
2020-12-13 18:02:13 +0700

    tugas3_Android

--- app/src/main/java/com/lisaaprilia/lalisashop/MainActivity.kt
+++ app/src/main/java/com/lisaaprilia/lalisashop/MainActivity.kt
@@ -1,26 +1,17 @@
 package com.lisaaprilia.lalisashop
 
-import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-
+import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val lbl_nama = findViewById<TextView>(R.id.lbl_nama)
-        val lbl_followers= findViewById<TextView>(R.id.lbl_followers)
-        val btn_follow = findViewById<Button>(R.id.btn_follow)
-        var followers = 1000
-
-        lbl_followers.text = followers.toString()
-        btn_follow.setOnClickListener {
-            followers++
-            lbl_followers.text = followers.toString()
-        }
+        setContentView(R.layout.relative_layout)
+        setContentView(R.layout.constraint_layout)
+        setContentView(R.layout.belajar_linear_layout)
+        setContentView(R.layout.belajar_constraint_layout)
 
     }
 }
